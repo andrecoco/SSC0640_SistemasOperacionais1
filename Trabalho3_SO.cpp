8c38abd48195917b14bff68232322b4fe611fd43
André Viana <andrebermudesviana@gmail.com>
2020-07-06 14:28:37 -0300

    acho que tá gg

--- Trabalho3_SO.cpp
+++ Trabalho3_SO.cpp
@@ -8,44 +8,7 @@
 using namespace std;
 
 //DEFINES
-#define ALG_SUBSTITUICAO "RELOGIO" //LRU ou RELOGIO
-
-
-/*
-    Ex: TAM_PAGINA 1024
-        TAM_MEMORIA_SECUNDARIA 32 * TAM_PAGINA
-        tamanho total = 32,768 (dá pra representar com 15 bits)
-        como a pagina tem 1024 endereços (representados com 10 bits), temos
-                  endereço (15 bits): 00101 1001011010
-                  n_pagina (5 bits) : 00101
-        endereço na pagina(10 bits) :       1001011010
-
-        Com isso só pelo endereço dá pra saber o número da página e o endereço na página :)
-*/
-
-/* OBS:
-A gente pode definir os metodos dentro da classe, ou fora.
-Ex:
-class nomeClasse{
-    void nomeMetodo(){
-        //
-    }
-}
-
-Ou dá pra fazer:
-class nomeClasse{
-    void nomeMetodo();
-}
-void nomeClasse::nomeMetodo(){
-        //
-    }
-*/
-
-/* OBS2: (Se quiser manter o msm estilo de nome das coisas)
-Eu usei tudo maiusculo pros defines -> NOME_DEFINE
-Pras classes eu coloquei a primeira letra maiscula -> NomeClasse
-Pras variaveis eu usei -> nomeVar
-*/
+#define ALG_SUBSTITUICAO "LRU" //LRU ou RELOGIO
 
 class Emulador { //seria o SO, todos os comandos passam por essa classe, ela distribui o resto
     bool useRelogio; //define se é relógio ou LRU
@@ -81,12 +44,17 @@ public:
         numBitsEnderecoLogico = (int)ceil(log2(TAM_MEMORIA_SECUNDARIA));
         numBitsIDPagina = numBitsEnderecoLogico - (int)ceil(log2(TAM_PAGINA));
 
-        cout << "############ EMULADOR ############" << endl;
+        cout << "############################## EMULADOR ############################" << endl;
         cout << "O endereco logico possui " << numBitsEnderecoLogico << "bits" << endl;
         cout << "Os primeiros " << numBitsIDPagina << "bits representam o numDaPagina" << endl;
+        if (useRelogio) {
+            cout << "O algoritmo de substituicao e o Relogio" << endl;
+        }
+        else {
+            cout << "O algoritmo de substituicao e o LRU" << endl;
+        }
+        cout << "####################################################################" << endl << endl;
 
-        //cout << numBitsEnderecoLogico << " " << numBitsIDPagina << endl;
-        //cout << binary2Decimal("100111") << endl;
     }
 
     //Retira uma pagina da RAM a partir de um algoritmo de substituicao
@@ -229,11 +197,20 @@ public:
         itr->tabelaPaginas[NumPaginaNoProcesso].numMoldura = numMoldura;
 
         //adiciona na lista de paginas na RAM
-        paginasAlocadasNaRAM.push_back(pair<int, int>(itr->id, IDPaginaVirtual));
-
-        //Se a RAM estivesse vazia, o ponteiro estará errado
-        if (paginasAlocadasNaRAM.size() == 1) {
-            ponteiro = paginasAlocadasNaRAM.begin();
+        if (!useRelogio) { //LRU
+            paginasAlocadasNaRAM.push_back(pair<int, int>(itr->id, IDPaginaVirtual));
+        }
+        else { //Relogio
+            //insere a pagina na posicao anterior a do ponteiro
+            if (paginasAlocadasNaRAM.size() == 1) {//Se a RAM estivesse vazia, o ponteiro n estará setado
+                paginasAlocadasNaRAM.push_back(pair<int, int>(itr->id, IDPaginaVirtual));
+                ponteiro = paginasAlocadasNaRAM.begin();
+            }
+            else {
+                list<pair<int, int>>::iterator posicao = ponteiro;
+                posicao--;
+                paginasAlocadasNaRAM.insert(posicao, pair<int, int>(itr->id, IDPaginaVirtual));
+            }
         }
 
         return 1; 
@@ -247,9 +224,12 @@ public:
         int PID;
         char command;
         list<Processo>::iterator itr;
+        list<pair<int, int>>::iterator itrLista;
         vector<int> novasPaginas;
         bool processoExiste;
         int IDMoldura;
+        int IDPagina;
+        int numPagina;
 
 
         cout << "Emulador: Começando a execução do simulador!" << endl;
@@ -278,8 +258,6 @@ public:
             arg2 = entrada.substr(indexesOfSpaces[0] + 1, indexesOfSpaces[1] - indexesOfSpaces[0] - 1);
             arg3 = entrada.substr(indexesOfSpaces[1] + 1, entrada.size() - (indexesOfSpaces[1] + 1));
 
-            //cout << "1- |" << arg1 << "| 2- |" << arg2 << "| 3- |" << arg3 << "|" << endl;
-
             //PID EH O NUMERO DO PROCESSO
             PID = stoi(arg1.substr(1, arg1.size() - 1));
 
@@ -287,8 +265,6 @@ public:
             command = arg2[0];
 
             //ARG3 PODE SER O ENDERECO OU TAMANHO, VOU MANTER COMO STRING
-
-            //cout << "PID- |" << PID << "| COMANDO- |" << command << "| NUMERO- |" << arg3 << "|" << endl;
 
             //C, R, W, P , I
             switch (command) {
@@ -320,11 +296,6 @@ public:
                         processos.push_back(*novo);
                     }
 
-                    break;
-
-                //Matar processo (n sei se precisa fazer)
-                case 'K':
-                    //TODO
                     break;
                 
                 //Ler Memoria
@@ -359,7 +330,24 @@ public:
                         break;
                     }
 
-                    //LE NA RAM (OU SO PRINTA KK)
+                    //LE NA RAM
+                    //marca que a pagina foi referenciada
+                    numPagina = binary2Decimal(arg3.substr(0, numBitsIDPagina));
+                    itr->tabelaPaginas[numPagina].R = true;
+
+                    //se for LRU, coloca ela no final
+                    if (!useRelogio) {
+                        IDPagina = itr->tabelaPaginas[numPagina].IDPagina;
+                        for (itrLista = paginasAlocadasNaRAM.begin(); itrLista != paginasAlocadasNaRAM.end(); itrLista++) {
+                            if (itrLista->second == IDPagina) {
+                                paginasAlocadasNaRAM.push_back(pair<int,int>(itrLista->first, itrLista->second));
+                                paginasAlocadasNaRAM.erase(itrLista);
+                                break;
+                            }
+                        }
+                    }
+
+                    //printa
                     cout << "Emulador: Processo " << PID << " leu no endereço " << arg3 << " que está na moldura " << IDMoldura << endl;
 
                     break;
@@ -396,7 +384,14 @@ public:
                         break;
                     }
 
-                    //ESCREVE NA RAM (OU SO PRINTA KK)
+                    //ESCREVE NA RAM
+                    //marca que a pagina foi referenciada e modificada
+                    numPagina = binary2Decimal(arg3.substr(0, numBitsIDPagina));
+                    IDPagina = itr->tabelaPaginas[numPagina].IDPagina;
+                    itr->tabelaPaginas[numPagina].R = true;
+                    itr->tabelaPaginas[numPagina].M = true;
+
+                    //printa
                     cout << "Emulador: Processo " << PID << " escreveu no endereço " << arg3 << " que está na moldura " << IDMoldura << endl;
 
                     break;
@@ -454,52 +449,3 @@ int main() {
 
     return 0;
 }
-
-/*
-QUESTION
-
-quando criamos um procesos, quantas páginas reais dele alocamos?
-podemos fazer "por demanda", só carrega quando precisar
-
-*/
-
-
-/*
-Como funcionaria?
-
-A classe emulador seria onde tudo acontece, ela recebe os comandos, faz os prints, e ela cuida de chamar
-tudo que precisa, ela seria tipo o "main".
-
-Ex1: comando P1 C 10 (criar processo de tam 10, pode ser 10 paginas)
-    o emulador chamaria algum metodo da Memoria virtual pra ver se tem espaço sobrando (pq isso é tipo o disco, se
-    n tiver espaço n dá pra criar mais programa kk). Se n tiver, n sei oq fazer, talvez avisar erro e é isso.
-    Se tiver espaço, algum método da memoria virtual alocaria as paginas e retornaria os IDs delas, aí o Emulador
-    criaria um Processo e passaria pra ele os IDs das paginas, o tamanho e o ID do processo em si, aí o construtor
-    do processo se vira em organizar a tabela de páginas lá kk.
-    E acho que isso resolve.
-
-Ex2: comando P1 R 0
-    o Emulador verifica se o processo 1 existe na lista de processos, se n existir, dá erro.
-    Se existir, ele pede pro Processo acessar o endereço (que tá na tabela lá certin).
-        Caso 1: endereço fora da região do programa -> printa erro e é isso
-        Caso 2: endereço válido, e tá na memPrincipal -> acessa a página (por um metódo da memória sla) e printa os dados
-        Caso 3: endereço válido, mas n tá na memPrincipal -> dá o page fault.
-
-        Minha dúvida é, quem lida com o page fault? o Emulador? o Processo? a Memoria?
-        (eu acho que o Emulador poderia lidar, simulando o SO, aí ele chamaria o algoritmo de substituição (que pode receber como argumento
-        a tabela de páginas do processo, pra decidir qual delas pode sair da Principal, mas aí estaria limitado em só remover páginas do mesmo
-        processo), podemos aceitar essa limitação, ou podemos fazer tipo tenta retirar dele, se n der tenta de outro processo, e vai tentando até conseguir liberar alguma moldura)
-        Depois de liberar chama o método que deu o page fault, mas agr vai funcionar pq tá em RAM.
-
-Algoritmos de Substituição:
-
-    LRU:   Tira a menos utilizada recentemente, minha ideia é, cada vez que for ler ou escrever numa pagina, retira ela da lista (q lista? aaaaaaa) e insere no final, aí sempre que precisar
-            só remover a primeira da lista (pq ela é a que tá a mais tempo sem ser utilizada).
-
-    Relogio: Aqui precisa de um bit R e um ponteiro, mas a ideia da lista é semelhante da do LRU. (mas fazer ela circular aqui)
-
-Vamos usar política de substituição global, páginas de qualquer processo podem ser retiradas.
-
-Slide 7 da Aula20, processo criado é todo copiado para área de troca do disco, e suas páginas serão carregadas para RAM quando necessárias.
-
-*/
